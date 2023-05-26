83fc804e5cfc422250c59d21d483351cd31631cc
restas13 <72560360+restas13@users.noreply.github.com>
2023-05-25 18:31:07 -0700

    adding the start to the server.js file and the connection.js. as well as adding lodash and axios

---
+++ Develop/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./routes');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
