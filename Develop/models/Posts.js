d92fd645120a9a768a12385244c5af17d4de14d3
restas13 <72560360+restas13@users.noreply.github.com>
2023-05-30 20:38:12 -0700

    adding models to test

---
+++ Develop/models/Posts.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model { }
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        message: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        author_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'gallery',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post;
