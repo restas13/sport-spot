5a7b2e0919118f830a44d8558b0540e3878ef9f4
Gdel-J <jessygdelvecchio@gmail.com>
2023-06-06 17:16:10 -0400

    render the page

--- public/js/discussion.js
+++ public/js/discussion.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
-    $('.discussion-button').on('click', function() {
-      const gameId = $(this).data('game-id');
-      window.location.href = `/discussion/${gameId}`;
+ document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll('.discussion');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            const gameId = this.getAttribute('data-game-id');
+            window.location.href = `/discussion/${gameId}`;
+        });
     });
-  });
-  
+});
