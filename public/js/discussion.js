262880aeeefc68a4d1632f640dadf3da553454e6
Gdel-J <jessygdelvecchio@gmail.com>
2023-06-07 14:15:36 -0400

    comments on discussion page

--- public/js/discussion.js
+++ public/js/discussion.js
@@ -1,71 +1,64 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Get gameId from URL
-  const urlParts = window.location.pathname.split('/');
-  const gameId = urlParts[urlParts.length - 1];
-
-  // Fetch posts and render them
-  fetch(`/api/posts/${gameId}`)
-      .then(response => response.json())
-      .then(posts => {
-          const postsContainer = document.getElementById('posts-container');
-
-          posts.forEach(post => {
-              const postElement = createPostElement(post);
-              postsContainer.appendChild(postElement);
-          });
-      });
-
-  // This is a helper function to create post HTML structure
-  function createPostElement(post) {
-      const postDiv = document.createElement('div');
-
-      const title = document.createElement('h2');
-      title.textContent = post.title;
-      postDiv.appendChild(title);
-
-      const content = document.createElement('p');
-      content.textContent = post.content;
-      postDiv.appendChild(content);
-
-      // Create update form
-      const updateForm = document.createElement('form');
-      updateForm.action = '#';
-      updateForm.method = 'POST';
-
-      const updateTitleInput = document.createElement('input');
-      updateTitleInput.type = 'text';
-      updateTitleInput.name = 'title';
-      updateTitleInput.value = post.title;
-      updateTitleInput.required = true;
-      updateForm.appendChild(updateTitleInput);
-
-      const updateContentInput = document.createElement('textarea');
-      updateContentInput.name = 'content';
-      updateContentInput.textContent = post.content;
-      updateContentInput.required = true;
-      updateForm.appendChild(updateContentInput);
-
-      const updateButton = document.createElement('button');
-      updateButton.type = 'submit';
-      updateButton.textContent = 'Update';
-      updateForm.appendChild(updateButton);
-
-      postDiv.appendChild(updateForm);
-
-      // Create delete form
-      const deleteForm = document.createElement('form');
-      deleteForm.action = '#';
-      deleteForm.method = 'POST';
-
-      const deleteButton = document.createElement('button');
-      deleteButton.type = 'submit';
-      deleteButton.textContent = 'Delete';
-      deleteForm.appendChild(deleteButton);
-
-      postDiv.appendChild(deleteForm);
-
-      return postDiv;
-  }
+// Add event listener to button
+document.getElementById("discussion-btn").addEventListener('click', function () {
+    openDiscussionPage("1234"); // replace with the actual game ID
 });
 
-  
+function openDiscussionPage(gameId) {
+    // Clean the body first
+    document.body.innerHTML = '';
+
+    // Create and append the discussion page title
+    let discussionPageDiv = document.createElement("div");
+    discussionPageDiv.textContent = `Discussion Page for Game ID: ${gameId}`;
+    document.body.appendChild(discussionPageDiv);
+
+    // Create and append the form to create new posts
+    let newPostForm = document.createElement("form");
+    newPostForm.action = '#';
+    newPostForm.method = 'POST';
+
+    let titleInput = document.createElement("input");
+    titleInput.type = 'text';
+    titleInput.name = 'title';
+    titleInput.placeholder = 'Title';
+    titleInput.required = true;
+    newPostForm.appendChild(titleInput);
+
+    let contentInput = document.createElement("textarea");
+    contentInput.name = 'content';
+    contentInput.placeholder = 'Content';
+    contentInput.required = true;
+    newPostForm.appendChild(contentInput);
+
+    let postButton = document.createElement("button");
+    postButton.type = 'submit';
+    postButton.textContent = 'Post';
+    newPostForm.appendChild(postButton);
+
+    document.body.appendChild(newPostForm);
+
+    // Create and append a container for posts
+    let postsContainer = document.createElement("div");
+    postsContainer.id = 'posts-container';
+    document.body.appendChild(postsContainer);
+
+    // Assuming that you have a fetchPosts function to load posts
+    fetchPosts(gameId);
+}
+
+function fetchPosts(gameId) {
+    fetch(`/api/posts/${gameId}`)
+    .then(response => response.json())
+    .then(posts => {
+        const postsContainer = document.getElementById('posts-container');
+
+        posts.forEach(post => {
+            const postElement = createPostElement(post);
+            postsContainer.appendChild(postElement);
+        });
+    });
+}
+
+function createPostElement(post) {
+    // Define your createPostElement function here
+}
